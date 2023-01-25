58d3212998f66caf7f4197693c0a5577746558cc
SimoLomba53 <simolomba253@gmail.com>
2023-01-25 16:20:32 +0100

    blocchi con colori diversi

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@
 //CREARE 100 QUADRATI
 //SUDDIVIDERLI IN BLU,FLIZZ MULTIPLI DI 3,BLIZZ MULTIPLI DI 5
 
-const ListItem = document.getElementById("lista")
-console.log(ListItem);
+const listItem = document.getElementById("lista")
+console.log(listItem);
 
 
-for (let i=0; i < 100 ;i++){
+for (let i=1; i < 100 ;i++){
     const square=document.createElement("li");
     console.log(square);
     square.innerHTML=i;
-    ListItem.append(square);
     square.classList.add("box");
+    listItem.append(square);
+    
+
+    if(i % 3 == 0) {
+      square.classList.add("green")
+    }
+    if(i % 5==0 ) {
+        square.classList.add("yellow")
+    }
+    else{
+        square.classList.add("blue")
+    }
 }
