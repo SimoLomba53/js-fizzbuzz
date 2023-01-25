18bfea9157b139a92cc4cd54365ea0590facced7
SimoLomba53 <simolomba253@gmail.com>
2023-01-25 15:27:27 +0100

    problema import html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//COLLEGARE I VARI QUADRATI ALLE PROPRIE CLASSI
+//CREARE 100 QUADRATI
+//SUDDIVIDERLI IN BLU,FLIZZ MULTIPLI DI 3,BLIZZ MULTIPLI DI 5
+
+const ListItem = document.getElementById("lista")
+console.log(ListItem);
+
+
+for (let i=0;i<100;i++){
+    const square=document.createElement("li");
+    console.log(square);
+    square.innerHTML=i;
+    ListItem.append(square);
+    square.classList("box");
+}
